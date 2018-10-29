82b7e9017d20efb2a27ee067d3799b5f8aae4fb9
Quang Anh <herost198@gmail.com>
2018-10-29 11:22:34 +0700

    init

---
+++ HowTo/Accordion/Create An Accordion/1.js
@@ -0,0 +1,9 @@
+var acc=document.getElementsByClassName("accordion");
+var i;
+for( i=0;i<acc.length;i++){
+    acc[i].addEventListener("click",function(){
+        this.classList.toggle("active");
+        var panel= this.nextElementSibling;
+        panel.style.display==='block'?panel.style.display="none":panel.style.display="block";
+    });
+}
